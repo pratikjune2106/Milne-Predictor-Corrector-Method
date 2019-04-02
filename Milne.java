2d54816d0519de57af29fdc166bf1f59e5a7e67a
Satvik <49191921+sciondev96@users.noreply.github.com>
2019-04-02 16:32:50 +0530

    Update Milne.java

--- Milne.java
+++ Milne.java
@@ -27,26 +27,27 @@ public class Milne
 		xvalue[i]=lower;
 		for(int j=1;j<n;j++)
 		{
-			xvalue[j]=(xvalue[j-1]+h);
+			xvalue[j]=(xvalue[j-1]+h);		//used to calculate range of x values
 		}
 		System.out.println("Enter the values of y: ");
 		for(int j=0;j<n;j++)
 		{
-			yvalue[j]=sc.nextDouble();			
+			yvalue[j]=sc.nextDouble();		//used to accept y values			
 		}
-		sc.nextLine();	
+		sc.nextLine();		//used to accept endline token after number input
 		System.out.println("Enter the equation: ");
 		String eqn=sc.nextLine();
-		Convert c=new Convert(eqn);
+		Convert c=new Convert(eqn);		//used to convert equation in infix form to postfix notation
 		//System.out.println(c.doTrans());
-		Evaluate e=new Evaluate();
+		Evaluate e=new Evaluate();		//used to evaluate the postfix equation
 		for(int j=0;j<n;j++)
 		{
-			function[j]=e.calculate(c.doTrans(),xvalue[j],yvalue[j]);
+			function[j]=e.calculate(c.doTrans(),xvalue[j],yvalue[j]);		//used to calculate y' values
 		}
 		System.out.println("y' values for the given equation "+eqn);
 		for(int j=0;j<n;j++)
 			System.out.println(df.format(function[j]));
+		//MILNE PREDICTOR CORRECTOR METHOD FORMULA
 		result=yvalue[0]+((4*h/3)*((2*function[1])-(function[2])+(2*function[3])));
 		System.out.println("y4p: "+df.format(result));
 		function[4]=e.calculate(c.doTrans(),search,result);
